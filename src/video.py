53234192c664ee9b452a4cee164c678189de596a
Ayrat Fatykhov <airat_91_91@mail.ru>
2023-12-14 22:54:02 +0300

    add exception at unknown links

--- src/video.py
+++ src/video.py
@@ -1,24 +1,25 @@
-import os
-
-from googleapiclient.discovery import build
-
-API_KEY: str = os.getenv('YT_API_KEY')
+from src.apimixin import APIMixin
 
 
-class Video:
+class Video(APIMixin):
     """Класс для видео из ютуба"""
 
     def __init__(self, video_id):
         """Экземпляр инициализируется по id видео. Дальше все данные будут подтягиваться по API"""
         self.video_id = video_id
-        youtube = self.get_service()
-        self.video = youtube.videos().list(part="snippet,statistics", id=video_id).execute()
-        snippet = self.video["items"][0]["snippet"]
-        statistics = self.video["items"][0]["statistics"]
-        self.video_name = snippet["title"]
-        self.video_url = snippet["thumbnails"]["default"]["url"]
-        self.video_view = statistics["viewCount"]
-        self.video_like = statistics["likeCount"]
+        try:
+            youtube = self.get_service()
+            self.video = youtube.videos().list(part="snippet,statistics", id=video_id).execute()
+            snippet = self.video["items"][0]["snippet"]
+            statistics = self.video["items"][0]["statistics"]
+            self.video_name = snippet["title"]
+            self.video_url = snippet["thumbnails"]["default"]["url"]
+            self.video_view = statistics["viewCount"]
+            self.video_like = statistics["likeCount"]
+        except IndexError:
+            self.video_name = None
+            self.video_view = None
+            self.video_like = None
 
     def __repr__(self):
         """Выводит данные, полученные по API"""
@@ -27,11 +28,6 @@ class Video:
     def __str__(self):
         """Выводит название видео"""
         return self.video_name
-
-    @classmethod
-    def get_service(cls):
-        """Возвращает объект для работы с YouTube API"""
-        return build('youtube', 'v3', developerKey=API_KEY)
 
 
 class PLVideo(Video):
